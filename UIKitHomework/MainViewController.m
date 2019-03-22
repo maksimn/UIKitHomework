78563f698fcf3d83f4a01a2c2afe22a6a692b7c0
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2019-03-22 22:37:25 +0300

    [feature] add refresh button

--- UIKitHomework/MainViewController.m
+++ UIKitHomework/MainViewController.m
@@ -10,15 +10,31 @@
 
 @interface MainViewController ()
 
+@property (nonatomic, strong) UIButton *refreshButton;
+
 @end
 
 @implementation MainViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     // Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = [UIColor greenColor];
+    
+    [self setupRefreshButton];
+    
 }
 
+- (void) setupRefreshButton
+{
+    self.refreshButton = [[UIButton alloc] initWithFrame:CGRectMake(50,60,120,40)];
+    
+    [self.refreshButton setTitle:@"Обновить" forState:UIControlStateNormal];
+    [self.refreshButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    self.refreshButton.backgroundColor = [UIColor whiteColor];
+    
+    [self.view addSubview:self.refreshButton];
+}
 
 @end
