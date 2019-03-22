dc4a314e9c3e4f7b1618f3c6d26977c282f9ee3e
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2019-03-22 21:17:20 +0300

    init

---
+++ UIKitHomework/MainViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  UIKitHomework
+//
+//  Created by Maksim Ivanov on 22/03/2019.
+//  Copyright © 2019 Maksim Ivanov. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor greenColor];
+}
+
+
+@end
