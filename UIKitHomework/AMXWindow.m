ef6101773b865db4caaf70bbf3d967610540f050
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2019-03-29 17:32:46 +0300

    remove isStartedAtLeftBorder

--- UIKitHomework/AMXWindow.m
+++ UIKitHomework/AMXWindow.m
@@ -19,21 +19,7 @@
     return self;
 }
 
-BOOL isStartedAtLeftBorder = NO;
 double previousX;
-
--(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
-{
-    UITouch *touch = touches.anyObject;
-    CGPoint point = [touch locationInView:self];
-    double screenWidth = self.frame.size.width;
-    double x0MaxValue = screenWidth / 10;
-    
-    if (point.x < x0MaxValue)
-    {
-        isStartedAtLeftBorder = YES;
-    }
-}
 
 -(void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
@@ -41,18 +27,13 @@ double previousX;
     CGPoint point = [touch locationInView:self];
     double screenWidth = self.frame.size.width;
     
-    if (isStartedAtLeftBorder && point.x >= previousX)
+    if (point.x >= previousX)
     {
         double colorValue = point.x / screenWidth;
         self.backgroundColor = [UIColor colorWithRed:colorValue green:colorValue blue:colorValue alpha:1.0];
     }
     
     previousX = point.x;
-}
-
--(void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
-{
-    isStartedAtLeftBorder = NO;
 }
 
 -(void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
